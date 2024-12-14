ce5a2cff9931de3c82593e3475f118c259742532
KJZ007 <itchyfeetflicks@gmail.com>
2024-12-14 21:25:14 +0530

    Ignore

--- lib/profile_page.dart
+++ lib/profile_page.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
-import '../models/activity_item.dart';
+import 'package:app/models/activity_item.dart';
 import '../services/user_service.dart';
 import '../screens/interests_page.dart';
 
